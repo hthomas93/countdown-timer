6ef478f8e412e4dcd805d4203c60836efc1bdf44
Harrison Thomas <hthomas146@utexas.edu>
2020-03-29 13:22:23 -0500

    Countdown component displays current date/time

---
+++ countdown-timer/src/components/Countdown/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+let time = new Date().toLocaleString();
+
+
+function Countdown(props) {
+    return (
+        <div>
+            <h1>Countdown</h1>
+            <p>The time is {time}</p>
+        </div>
+    )
+}
+
+export default Countdown;
